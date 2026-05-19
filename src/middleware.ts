07f02fdd2b3d3b838e07d0068fea76c03cb1f104
Liakhov <liahov.yura@gmail.com>
2026-05-19 22:15:46 +0300

    refactor: remove guard middleware

--- src/middleware.ts
+++
@@ -1,5 +0,0 @@
-import { sequence } from 'astro:middleware';
-import { guard } from '@/lib/analytics/guard';
-import { track } from '@/lib/analytics/track';
-
-export const onRequest = sequence(guard, track);
